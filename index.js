1102014264b481e155047eeffb2534e2a60b4000
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-11-17 19:49:22 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, rev)
+{
+    return drivers.filter(function(driver) { return driver["revenue"] >= rev} ) ;
+}
+
+function driverNamesWithRevenueOver(drivers, rev)
+{
+    return driversWithRevenueOver(drivers, rev).map (function(driver) { return driver["name"] } );
+}
+
+function exactMatch(drivers, hash)
+{
+    return drivers.filter(function(driver) { return hash["name"] === driver["name"] || hash["revenue"] === driver["revenue"]});
+}
+
+function  exactMatchToList(drivers, hash)
+{
+    return exactMatch(drivers, hash).map(function(driver) {return driver["name"]});
+}
